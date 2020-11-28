4102c8d037675034a3a6ca801be71ffbdff69ca0
Ayan Sinha <ayan.sinha007@gmail.com>
2020-11-28 22:52:11 +0530

    feat: add packages and files
    desc: added structure of package and files(layouts and drawables) and fonts

--- app/src/main/java/org/weathercast/ui/ActivityForecast.kt
+++ app/src/main/java/org/weathercast/ui/ActivityForecast.kt
@@ -1,9 +1,10 @@
-package org.weathercast
+package org.weathercast.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.weathercast.R
 
-class MainActivity : AppCompatActivity() {
+class ActivityForecast: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
